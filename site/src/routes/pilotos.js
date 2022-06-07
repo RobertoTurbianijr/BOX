bcf3ba88da7d052911d6eb39c1b7e6d14ea0dffc
Roberto Turbiani <turbianiroberto@gmail.com>
2022-06-07 08:35:02 -0300

    listar e veja quem é. ok

--- site/src/routes/pilotos.js
+++ site/src/routes/pilotos.js
@@ -1,23 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-var usuarioController = require("../controllers/usuarioController");
+var pilotoController = require("../controllers/pilotoController");
 
-router.get("/", function (req, res) {
-    usuarioController.testar(req, res);
-});
-
-router.get("/listar", function (req, res) {
-    usuarioController.listar(req, res);
-});
-
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
-router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
-})
-
-router.post("/autenticar", function (req, res) {
-    usuarioController.entrar(req, res);
+router.get("/listarPilotos", function (req, res) {
+    pilotoController.listarPilotos(req, res);
 });
 
 module.exports = router;
